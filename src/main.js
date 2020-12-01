34d0553d53cce7d589329d31648c35fe5eaf8e35
[aui-js] <[cl15095344637@163.com]>
2020-12-01 14:49:55 +0800

    edit

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import $mix from './mix-view'
 Vue.config.productionTip = false
 
-Vue.use($mix);
+import aui from 'aui-1.0'
+ 
+Vue.prototype.aui = aui
 
 new Vue({
   router,
